2b200ffa0c49d66a1f9b638ba29c916ccca66102
dwgabriel <31158951+dwgabriel@users.noreply.github.com>
2021-01-23 11:03:17 +0800

    Add files via upload

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,126 +1,87 @@
+
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.model.Bind;
+import com.github.dockerjava.api.model.AuthConfig;
+import com.github.dockerjava.api.model.Image;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
-import org.apache.commons.io.FilenameUtils;
+import com.github.dockerjava.core.command.PullImageResultCallback;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.attribute.UserDefinedFileAttributeView;
-import java.util.LinkedHashMap;
-import java.util.stream.Stream;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-/**
- * Created by Daryl Wong on 3/30/2019.
- */
 public class Test {
 
-    LinkedHashMap<String, String> taskAppInfo = new LinkedHashMap<>();
+    static File userHome = new File(System.getProperty("user.home"));
+    //    static File userHome = new File(System.getProperty("user.home"), "/Library/Application Support");
+    static File appData = new File(System.getenv("APPDATA"));
+    static File nebulaData = new File(appData, "Nebula");
+    static File taskCache = new File(nebulaData, "taskcache");
+    static File results = new File(taskCache, "results");
 
-    public static void main(String[] args) throws IOException {
+    private static DecimalFormat timeFormat = new DecimalFormat("#.##");
+    private static DefaultDockerClientConfig config;
+    private static DockerClient dockerClient;
+//    static final DbxRequestConfig dbxConfig = DbxRequestConfig.newBuilder("dropbox/nebula-render").build();
+//    static final DbxClientV2 client = new DbxClientV2(dbxConfig, "rM8fF-GuUNAAAAAAAAAAK6ksJER9acjYeF1krFbX63InD8wn_Iq-5fDlV_1YM6gh");
+    private static boolean updated = false;
 
-        setMetadata("blender");
-        getMetadata("blender");
-    }
-    public void addInfo(String taskID, String application) {
-        taskAppInfo.put(taskID, application);
-    }
-
-    public void printInfo() {
-        taskAppInfo.entrySet().forEach(entry -> {
-            System.out.println("TEST | " + entry.getKey() + " = " + entry.getValue());
-        });
-    }
-
-
-public static void startNebula () {
-
-    DefaultDockerClientConfig config
-            = DefaultDockerClientConfig.createDefaultConfigBuilder()
-            .withRegistryEmail("darylgabrielwong@gmail.com")
-            .withRegistryPassword("DWGabriel4")
-            .withRegistryUsername("darylgabrielwong")
-            .build();
-
-    DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
-
-    CreateContainerResponse container = dockerClient.createContainerCmd("ikester/blender")
-            .withCmd("bin/bash")
-            .withName("nebula_blender")
-            .withBinds(Bind.parse("/c/Users/Daryl Wong/desktop/nebula/code/nebulanode/taskcache:/test/"))
-            .withCmd("test/blendertest.blend", "-o", "test/frame_###", "-f", "1")
-            .exec();
-
-    dockerClient.startContainerCmd(container.getId()).exec();
-    }
-
-    public static void compute() {
-        File testFile = new File("C:\\Users\\Daryl Wong\\Desktop\\test\\bmwcpu.blend");
-        String fileExt = FilenameUtils.getExtension(testFile.getName());
-        System.out.println("File Extension : " + fileExt);
-    }
-
-    public static void setMetadata(String application) {
-        Path testPath = new File("C:\\Users\\Daryl Wong\\Desktop\\Misc\\mountainrange.jpg").toPath();
+    public static void main(String[] args) {
         try {
-//            Files.setAttribute(testPath, "user:application", application.getBytes());
-//            System.out.println("Attribute : " + Files.getAttribute(testPath, "user:application"));
+            
 
-            UserDefinedFileAttributeView view = Files
-                    .getFileAttributeView(testPath, UserDefinedFileAttributeView.class);
-            view.write("user:application", Charset.defaultCharset().encode("blender"));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void getMetadata(String application) {
-        Path testPath = new File("C:\\Users\\Daryl Wong\\Desktop\\Misc\\mountainrange.jpg").toPath();
-        try {
-            UserDefinedFileAttributeView view = Files
-                    .getFileAttributeView(testPath, UserDefinedFileAttributeView.class);
-            String name = "user:application";
-            ByteBuffer buffer = ByteBuffer.allocate(view.size(name));
-            view.read(name, buffer);
-            buffer.flip();
-            String value = Charset.defaultCharset().decode(buffer).toString();
-            System.out.println("Attribute : " + value);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    public static boolean pullBlender() throws InterruptedException {
+        boolean blenderImagePulled = false;
 
-    public static void listTaskCache() throws IOException {
+        config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost("tcp://192.168.99.100:2376")
+                .withDockerTlsVerify(true)
+                .withDockerCertPath(userHome + "/.docker/machine/certs")
+                .withDockerConfig(userHome + "/.docker")
+                .withRegistryEmail("darylgabrielwong@gmail.com")
+                .withRegistryUsername("darylgabrielwong")
+                .withRegistryPassword("DWGabriel4")
+                .build();
 
-        File taskCache = new File("C:\\Users\\Daryl Wong\\Desktop\\Nebula\\Code\\nebulanode\\taskcache");
+//        DockerHttpClient dockerHttpClient = new ApacheDockerHttpClient.Builder()
+//                .dockerHost(config.getDockerHost())
+//                .sslConfig(config.getSSLConfig()).build();
+//
+//        dockerClient = DockerClientImpl.getInstance(config, dockerHttpClient);
 
+        dockerClient = DockerClientBuilder.getInstance(config)
+                .build();
 
-        if (taskCache.listFiles().length <= 0) {
-            System.out.println("There are no tasks to compute at this time.");
-        } else if (taskCache.listFiles().length > 0) {
+        dockerClient.pullImageCmd("alpine")
+                .exec(new PullImageResultCallback())
+                .awaitCompletion(300, TimeUnit.SECONDS);
 
-            try (Stream<Path> fileStream = Files.walk(Paths.get(taskCache.getAbsolutePath()))) {
-                fileStream
-                        .filter(Files::isRegularFile)
-                        .map(Path::toFile)
-                        .forEachOrdered(file ->  {
-                            System.out.println(file.getName());
+         // TODO - HARDCODED, SHOULD NOT RELY ON TIME.
 
-                            try {
-                                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        });
+//        ListImagesCmd images = dockerClient.listImagesCmd();
+        List<Image> images = dockerClient.listImagesCmd().exec();
+        Iterator<Image> iterator = images.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next().toString());
+            if (iterator.next().toString().contains("blender")) {
+                blenderImagePulled = true;
             }
         }
+//        System.out.println("DOCKER IMAGE : " + images);
+//        if (images.toString().contains("blender")) {
+//            blenderImagePulled = true;
+//        }
+
+
+        return blenderImagePulled;
     }
+
 }
