acc71bd252be23f676f20a0da9f1a81d4381f963
dwgabriel <31158951+dwgabriel@users.noreply.github.com>
2021-07-06 09:50:42 +0800

    Delete src/main/java/java directory

--- src/main/java/java/Tester.java
+++
@@ -1,8 +0,0 @@
-public class Tester {
-
-    public String getSource() {
-        String nebulaDirPath = getClass().getProtectionDomain().getCodeSource().getLocation().getPath().replace("%20", "");
-
-        return nebulaDirPath;
-    }
-}
